1e016c49efbbac2ae4147e237f82978ee3a83fac
Hema Veeraragavathatham <hema.ragavan@gmail.com>
2024-05-16 21:28:09 -0700

    Switch to using HashRouter + Change homepage in package.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import About from "./components/About";
 import AllSongs from "./components/AllSongs.js";
 import Song from "./components/Song.js";
