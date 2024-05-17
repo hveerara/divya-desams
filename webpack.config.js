3881ddc2afaa766534a3c541ee79cba3552af1c1
Hema Veeraragavathatham <hema.ragavan@gmail.com>
2024-05-16 21:48:21 -0700

    [Debug vercel] Change location of index.html to root folder

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ module.exports = {
         collapseWhitespace: true,
         removeAttributeQuotes: true,
       },
-      template: "./public/index.html",
+      template: "./index.html",
     }),
     new CopyPlugin({
       patterns: [
